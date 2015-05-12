828ebd156cd663191f04417f76b9fe50a67e1ebc
unknown <janelson@CT156-26.adit.mines.edu>
2015-05-12 15:35:23 -0600

    initial commit, skeleton directory for engine

---
+++ LibGDX_Generate/desktop/src/org/TheGivingChild/Engine/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.TheGivingChild.Engine.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.TheGivingChild.Engine.TGC_Engine;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TGC_Engine(), config);
+	}
+}
