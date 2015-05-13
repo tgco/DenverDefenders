17748207ad26058f4ca88721f97e00bf4a4a95d9
unknown <janelson@CT156-26.adit.mines.edu>
2015-05-13 17:10:19 -0600

    Buttons added using button pack textures. Click events NOT added yet

--- LibGDX_Generate/core/src/org/TheGivingChild/Engine/TGC_Engine.java
+++ LibGDX_Generate/core/src/org/TheGivingChild/Engine/TGC_Engine.java
@@ -1,38 +1,94 @@
 package org.TheGivingChild.Engine;
 
+import java.util.ArrayList;
+
+
+import java.util.Iterator;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Button.ButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.ButtonGroup;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class TGC_Engine extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture mainScreen_Editor;
-	Texture mainScreen_Options;
-	Texture mainScreen_Play;
+	final static int BUTTON_STATES = 2;//corresponds to how many states each button has for the Buttons.pack textures pack.
+	//create the stage for our actors
+	Stage stage;
+	String[] buttonAtlasNamesArray = {"ButtonPressed_MainScreen_Play", "Button_MainScreen_Play", "ButtonPressed_MainScreen_Editor", "Button_MainScreen_Editor", "ButtonPressed_MainScreen_Options", "Button_MainScreen_Options"};
+	//fonts
+    BitmapFont font;
+    
+    //button styling	
+    TextButtonStyle buttonStyleMainMenyPlay;
+    TextButtonStyle buttonStyleMainMenyEditor;
+    TextButtonStyle buttonStyleMainMenyOptions;
+    Skin skin;
+    TextureAtlas buttonAtlas;
+    //button groups
+    ButtonGroup buttonGroupMainMenu;
+    //buttons
+    TextButton buttonMainMenuPlay;
+    TextButton buttonMainMenuOptions;
+    TextButton buttonMainMenuEditor;
+    //create tables for the UI
+    Table rootTable;
+    
+	//create needed textures
 	Texture mainScreen_Splash;
+	
 	//testing git pulls comment
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
+		stage = new Stage();
 		//create main menu images
-		mainScreen_Editor = new Texture("MainScreen_Editor.png");
-		mainScreen_Options = new Texture("MainScreen_Options.png");
-		mainScreen_Play = new Texture("MainScreen_Play.png");
-		mainScreen_Splash = new Texture("MainScreen_Splash.png");
+		Gdx.input.setInputProcessor(stage);
+		buttonGroupMainMenu = new ButtonGroup<Button>();
+		//initialize root Table
+		rootTable = new Table();
+		//button stuff
+        font = new BitmapFont();
+        skin = new Skin();
+        //make an atlas using the button texture pack
+        buttonAtlas = new TextureAtlas("Packs/Buttons.pack");
+        //define the regions
+        skin.addRegions(buttonAtlas);
+        //create a table for the buttons
+        Table table = new Table();
+        //variable to keep track of button height for table positioning
+        float buttonHeight = 0;
+        //iterate over button pack names in order to check 
+        for(int i = 0; i < buttonAtlasNamesArray.length-1; i+=BUTTON_STATES){
+        	TextButtonStyle bs = new TextButtonStyle();
+        	bs.font = font;
+        	bs.down = skin.getDrawable(buttonAtlasNamesArray[i]);
+        	bs.up = skin.getDrawable(buttonAtlasNamesArray[i+1]);
+        	TextButton b = new TextButton("", bs);
+        	b.setSize(Gdx.graphics.getWidth()/3, Gdx.graphics.getHeight()/3);
+        	table.add(b).size(Gdx.graphics.getWidth()/3, Gdx.graphics.getHeight()/3);
+        	buttonHeight = b.getHeight();
+        }
+        
+        rootTable.add(table);
+        rootTable.setPosition(Gdx.graphics.getWidth()/2, buttonHeight/2);
+        stage.addActor(rootTable);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		//draw main menu
-		batch.draw(mainScreen_Splash, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		batch.draw(mainScreen_Editor, Gdx.graphics.getWidth()*2/3, 0, Gdx.graphics.getWidth()/3, Gdx.graphics.getHeight()/3);
-		batch.draw(mainScreen_Options, Gdx.graphics.getWidth()/3, 0, Gdx.graphics.getWidth()/3, Gdx.graphics.getHeight()/3);
-		batch.draw(mainScreen_Play, 0, 0, Gdx.graphics.getWidth()/3, Gdx.graphics.getHeight()/3);
-		batch.end();
+		super.render();
+		stage.draw();
 	}
+	
 }
