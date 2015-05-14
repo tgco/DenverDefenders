b2b964fde273fdbaa7d8d73c731a99926fe18d65
unknown <janelson@CT156-26.adit.mines.edu>
2015-05-14 11:32:18 -0600

    Main menu buttons should switch to new screens

---
+++ LibGDX_Generate/core/src/org/TheGivingChild/Engine/ScreenLevelManager.java
@@ -0,0 +1,7 @@
+package org.TheGivingChild.Engine;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+public class ScreenLevelManager extends ScreenAdapter{
+	
+}
--- LibGDX_Generate/core/src/org/TheGivingChild/Engine/TGC_Engine.java
+++ LibGDX_Generate/core/src/org/TheGivingChild/Engine/TGC_Engine.java
@@ -1,32 +1,27 @@
 package org.TheGivingChild.Engine;
 
-import java.util.ArrayList;
-
-
-import java.util.Iterator;
-
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.Button.ButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.ButtonGroup;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
-import com.badlogic.gdx.utils.viewport.Viewport;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
-public class TGC_Engine extends ApplicationAdapter {
+public class TGC_Engine extends Game {
 	final static int BUTTON_STATES = 2;//corresponds to how many states each button has for the Buttons.pack textures pack.
 	//create the stage for our actors
 	Stage stage;
-	String[] buttonAtlasNamesArray = {"ButtonPressed_MainScreen_Play", "Button_MainScreen_Play", "ButtonPressed_MainScreen_Editor", "Button_MainScreen_Editor", "ButtonPressed_MainScreen_Options", "Button_MainScreen_Options"};
+	String[] buttonAtlasNamesArray = {"ButtonPressed_MainScreen_Play", "Button_MainScreen_Play",/* "ButtonPressed_MainScreen_Editor", "Button_MainScreen_Editor",*/ "ButtonPressed_MainScreen_Options", "Button_MainScreen_Options"};
+	ScreenAdapter[] screens = {new ScreenLevelManager(), new OptionsScreen()};
 	//fonts
     BitmapFont font;
     
@@ -36,8 +31,6 @@ public class TGC_Engine extends ApplicationAdapter {
     TextButtonStyle buttonStyleMainMenyOptions;
     Skin skin;
     TextureAtlas buttonAtlas;
-    //button groups
-    ButtonGroup buttonGroupMainMenu;
     //buttons
     TextButton buttonMainMenuPlay;
     TextButton buttonMainMenuOptions;
@@ -54,7 +47,6 @@ public class TGC_Engine extends ApplicationAdapter {
 		stage = new Stage();
 		//create main menu images
 		Gdx.input.setInputProcessor(stage);
-		buttonGroupMainMenu = new ButtonGroup<Button>();
 		//initialize root Table
 		rootTable = new Table();
 		//button stuff
@@ -68,18 +60,26 @@ public class TGC_Engine extends ApplicationAdapter {
         Table table = new Table();
         //variable to keep track of button height for table positioning
         float buttonHeight = 0;
-        //iterate over button pack names in order to check 
-        for(int i = 0, j = 0; i < buttonAtlasNamesArray.length-1; i+=BUTTON_STATES){
+        int widthDividider = (buttonAtlasNamesArray.length/2);
+        //iterate over button pack names in order to check
+        for(int i = 0; i < buttonAtlasNamesArray.length-1; i+=BUTTON_STATES){
         	TextButtonStyle bs = new TextButtonStyle();
         	bs.font = font;
         	bs.down = skin.getDrawable(buttonAtlasNamesArray[i]);
         	bs.up = skin.getDrawable(buttonAtlasNamesArray[i+1]);
         	TextButton b = new TextButton("", bs);
-        	b.setSize(Gdx.graphics.getWidth()/3, Gdx.graphics.getHeight()/3);
-        	table.add(b).size(Gdx.graphics.getWidth()/3, Gdx.graphics.getHeight()/3);
+        	b.setSize(Gdx.graphics.getWidth()/3, Gdx.graphics.getHeight()/widthDividider);
+        	table.add(b).size(Gdx.graphics.getWidth()/3, Gdx.graphics.getHeight()/widthDividider);
         	buttonHeight = b.getHeight();
+        	final int j = i;
+        	//button to transition to different screens.
+        	b.addListener(new ChangeListener(){
+				@Override
+				public void changed(ChangeEvent event, Actor actor) {
+					setScreen(screens[j/2]);
+				}
+        	});
         }
-        
         rootTable.add(table);
         rootTable.setPosition(Gdx.graphics.getWidth()/2, buttonHeight/2);
         stage.addActor(rootTable);
