23c314b6afa1fdc42d09cce978d06e8bb3625bac
Kevin Day <kday@mines.edu>
2015-05-18 11:41:10 -0600

    made progress with XML_Reader

--- LibGDX_Generate/core/src/org/TheGivingChild/Engine/GameObject.java
+++ LibGDX_Generate/core/src/org/TheGivingChild/Engine/GameObject.java
@@ -1,38 +1,64 @@
 package org.TheGivingChild.Engine;
 
 import java.awt.Point;
-import java.util.Map;
+import java.lang.reflect.Method;
 
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ObjectMap;
+import com.badlogic.gdx.utils.reflect.*;
+import com.badlogic.gdx.utils.reflect.Method.*;
+
 import org.TheGivingChild.Engine.Attributes.*;
 
 public class GameObject extends Actor{//libGDX actors have all the listeners we will need
-	//private Array<Attribute> attributes;
-	private Array<String> validAttributes;
+	private ObjectMap<String,Integer> validAttributes;
+	private ObjectMap<String,String> attributeValues;
+	private int ID;
 	
-	public GameObject(){
-		
+	public GameObject(int newID){
+		ID = newID;
+		validAttributes = new ObjectMap<String,Integer>();
+		attributeValues = new ObjectMap<String,String>();
+		System.out.println("\nNew object, ID: " + ID);
 	}
 	
 	public void update(){
-		
+		for(Method currentMethod:this.getClass().getMethods()){
+			if(validAttributes.containsKey(currentMethod.getName().replaceAll("attribute_",""))){
+				try{
+					currentMethod.invoke(this);
+					System.out.println("current method: " + currentMethod.getName());
+				}catch(Exception e){
+					System.out.println("Exception calling method " + currentMethod.getName() + " Exception: " + e);
+				}
+			}
+		}
 	}
 	
 	public void addValidAttribute(String newAttribute){
-		validAttributes.add(newAttribute);
+		validAttributes.put(newAttribute,1);
+		System.out.println(newAttribute);
 	}
 	
-	public void attribute_health(GameObject object){
+	
+	
+	
+	
+	
+	
+	
+	//ATTRIBUTES
+	
+	public void attribute_health(){
 		System.out.println("HEALTH");
 	}
 	
-	public void attribute_color(GameObject object){
+	public void attribute_color(){
 		System.out.println("COLOR");
 	}
 	
-	public void attribute_disappearsOnPress(GameObject object){
+	public void attribute_disappearsOnPress(){
 		System.out.println("DISAPPEARSONPRESS");
 	}
 }
