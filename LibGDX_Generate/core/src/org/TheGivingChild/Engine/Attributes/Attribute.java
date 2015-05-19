9b10424a9d4b94be6395d60d6c7019809c08cef7
mtzimour <mtzimour@CT156-16.adit.mines.edu>
2015-05-19 12:02:21 -0600

    added a generic timer class for evaluating game and level times

--- LibGDX_Generate/core/src/org/TheGivingChild/Engine/Attributes/Attribute.java
+++ LibGDX_Generate/core/src/org/TheGivingChild/Engine/Attributes/Attribute.java
@@ -2,6 +2,6 @@ package org.TheGivingChild.Engine.Attributes;
 
 import org.TheGivingChild.Engine.GameObject;
 
-public interface Attribute {
+public abstract interface Attribute {
 	public void update(GameObject gameObject);
 }
