0a47d672e00b85037075c162c35f653a9a2d567a
Kevin Day <kday@mines.edu>
2015-05-21 15:12:52 -0600

    XML_Reader compileLoseConditions completed

--- LibGDX_Generate/core/src/org/TheGivingChild/Engine/XML/LoseEnum.java
+++ LibGDX_Generate/core/src/org/TheGivingChild/Engine/XML/LoseEnum.java
@@ -18,10 +18,34 @@ public enum LoseEnum {
 		public void setValues(Array<String> newValues){
 			time = Integer.parseInt(newValues.first());
 		}
+		public boolean checkLose(){
+			return false;
+		}
+	},
+	COLLISIONWITHOBJECTLOSE{
+		private int objectID1;
+		private int objectID2;
+		public String getXMLDescription(){
+			return "collisionWithObjectLose";
+		}
+		public boolean checkLose(){
+			return false;
+		}
+		public void setValues(Array<String> newValues){
+			objectID1 = Integer.parseInt(newValues.get(0));
+			objectID2 = Integer.parseInt(newValues.get(1));
+		}
+		public Array<String> getValues(){
+			Array<String> temp = new Array<String>();
+			temp.add(objectID1+"");
+			temp.add(objectID2+"");
+			return temp;
+		}
 	};
 	public abstract String getXMLDescription();
 	public abstract Array<String> getValues();
 	public abstract void setValues(Array<String> newValues);
+	public abstract boolean checkLose();
 	
 	public static LoseEnum newType(String type){
 		return valueOf(type.toUpperCase());
