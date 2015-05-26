a09e5c299a3a8f48a4494accb7b3c3b653bef0d5
Nathaniel Jacobi <njacobi@mines.edu>
2015-05-26 11:19:01 -0600

    Rolling back since previous commit was a mistake

--- LibGDX_Generate/core/src/org/TheGivingChild/Engine/XML/Attribute.java
+++ LibGDX_Generate/core/src/org/TheGivingChild/Engine/XML/Attribute.java
@@ -6,8 +6,32 @@ import com.badlogic.gdx.utils.Array;
 
 public enum Attribute {
 	/* each type will have a update method and a setValues method which all take in an Array<String>
-	 * each type will have a field of variable type that is private
+	 * each type can have private fields
 	 */
+	MOVES{//hooray now things can move and other attributes can affect movement!, it's input arguments only affect initial speed. 
+		private boolean hasMoved = false;
+		private float[] velocity;
+		public void update(GameObject myObject){//gotta do some jank code because other objects need to see it's speed, but dont wanna pass around an Array<String> and convert back and forth with floats
+			System.out.println("\nmoves Update");
+			if(!hasMoved){
+				hasMoved = true;
+				myObject.setVelocity(velocity);
+			}
+			myObject.setPosition(myObject.getX() + (velocity[0]*Gdx.graphics.getDeltaTime()), myObject.getY() + (velocity[1]*Gdx.graphics.getDeltaTime()));
+		}
+		public void setValues(Array<String> newValues){
+			velocity = new float[2];
+			velocity[0] = Float.parseFloat(newValues.get(0));//x
+			velocity[1] = Float.parseFloat(newValues.get(1));//y
+		}
+		public Array<String> getValues(){
+			Array<String> temp = new Array<String>();
+			temp.add(velocity[0]+"");
+			temp.add(velocity[1]+"");
+			return temp;
+		}
+		public String getXMLName(){return "moves";}
+	},
 	HEALTH{
 		private int health;
 		public void update(GameObject myObject){
@@ -41,16 +65,28 @@ public enum Attribute {
 		}
 		public String getXMLName(){return "color";}
 	},
-	MOVESONSETPATH{
+	MOVESONSETPATH{//requires moves?
+		private double tolerance;
 		private Array<float[]> path;
+		private int currentPoint;
 		public void update(GameObject myObject){
 			System.out.println("\nMovesOnSetPath Update");
-			for(float[] currentPoint:path)
-				System.out.println(currentPoint[0] + ", " + currentPoint[1]);
+			if(distanceToPoint(myObject.getX(),myObject.getY()) < tolerance){//if 'close enough' to target point, goto next ((x2-x1)^2 + (y2-y1)^2)^.5
+				currentPoint++;
+				//FINISH ME BRAH I NEED TO GO THE RIGHT DIRECTION, sin(theta) = slope
+				
+			}
+		}
+		
+		private double distanceToPoint(float x, float y){
+			return Math.pow(Math.pow(x-path.get(currentPoint)[0], 2) + Math.pow(y-path.get(currentPoint)[1],2),.5);
 		}
 		
 		public void setValues(Array<String> newValues){
 			path = stringToPath(newValues.get(0));
+			tolerance = Double.parseDouble(newValues.get(1));
+			currentPoint = 0;//initializes for the object to immediately start heading for first point
+			//NEEDS TO CALCULATE THE INITIAL DIRECTION TOO, OH BOYYYY
 		}
 		
 		public Array<String> getValues(){
@@ -59,6 +95,7 @@ public enum Attribute {
 			for(float[] point:path)
 				tempS+=";" + point[0] + "," + point[1];
 			temp.add(tempS.replaceFirst(";",""));
+			temp.add(tolerance+"");
 			return temp;
 		}
 		
@@ -86,7 +123,7 @@ public enum Attribute {
 	},
 	FALLSATSETRATE{
 		private int rate;
-		public void update(GameObject myObject){
+		public void update(GameObject myObject){//will probably need to change this to something like the moves attribute
 			System.out.println("\nfallsAtSetRate Update");
 			myObject.setPosition(myObject.getX(), myObject.getY() - rate * (Gdx.graphics.getDeltaTime()));
 		}
