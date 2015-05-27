52006922cc0a636ec0f35ac84cad243b5a52a307
JackWesleyNelson <janelson@mymail.mines.edu>
2015-05-26 18:41:50 -0600

    objects now are clickable within a 100x100 area, should be changed to be based on texture size. added enum for destroy on click, this simply syso's a statement noting the destruction when clicked.

--- LibGDX_Generate/core/src/org/TheGivingChild/Engine/XML/GameObject.java
+++ LibGDX_Generate/core/src/org/TheGivingChild/Engine/XML/GameObject.java
@@ -1,21 +1,14 @@
 package org.TheGivingChild.Engine.XML;
 
-import java.lang.reflect.Method;
-
-import org.TheGivingChild.Engine.UserInputListener;
-import org.TheGivingChild.Engine.UserInputProcessor;
+import org.TheGivingChild.Engine.Attributes.InputListenersEnums;
 
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.EventListener;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.ObjectMap;
-import com.badlogic.gdx.utils.reflect.*;
-import com.badlogic.gdx.utils.reflect.Method.*;
 
-//GameObject is essentially a sotrage container for all the information associated with each object on the screen
+//GameObject is essentially a storage container for all the information associated with each object on the screen
 public class GameObject extends Actor{//libGDX actors have all the listeners we will need
 	private int ID;
 	private String imageFilename;
@@ -33,9 +26,11 @@ public class GameObject extends Actor{//libGDX actors have all the listeners we 
 		imageFilename = img;
 		setPosition(newPosition[0],newPosition[1]);
 		attributes = new Array<Attribute>();
-
-
 		velocity = new float[] {0,0};
+		//should be set using the bounds of the texture rather than a static number
+		setBounds(getX(), getY(), 100, 100);
+		//add the destroy on click event
+		addListener(InputListenersEnums.DESTROY_ON_CLICK.getInputListener(this));
 
 	}
 				
