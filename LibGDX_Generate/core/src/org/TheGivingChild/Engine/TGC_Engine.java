1cdfae2962bde82c289cf725860d1cc224ceb665
Corey Tokunaga-Reichert <ctokunag@mines.edu>
2015-06-03 12:11:37 -0600

    Viewport scales everything.

--- LibGDX_Generate/core/src/org/TheGivingChild/Engine/TGC_Engine.java
+++ LibGDX_Generate/core/src/org/TheGivingChild/Engine/TGC_Engine.java
@@ -13,6 +13,7 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -22,10 +23,16 @@ import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class TGC_Engine extends Game {
 	final static int DESKTOP_WIDTH = 1024;
-	final static int DESKTOP_HEIGHT = 576;
+	final static int DESKTOP_HEIGHT = 768;
 	final static int BUTTON_STATES = 2;//corresponds to how many states each button has for the Buttons.pack textures pack.
 	//create the stage for our actors
 	private TGC_Stage stage;
@@ -52,6 +59,10 @@ public class TGC_Engine extends Game {
     private XML_Writer writer;
     //Map stuff
     private OrthographicCamera mapCamera;
+    
+    //Viewport stuff
+    OrthographicCamera camera;
+    Viewport viewport;
    
 	public void addLevels(Array<Level> levels){
 			this.levels.addAll(levels);
@@ -147,6 +158,13 @@ public class TGC_Engine extends Game {
 		float h = Gdx.graphics.getHeight();
 		mapCamera = new OrthographicCamera();
 		mapCamera.setToOrtho(false, w, h);
+		
+		//Viewport stuff
+		camera = new OrthographicCamera();
+		viewport = new StretchViewport(16, 9, camera);
+		viewport.apply();
+		camera.position.set(camera.viewportWidth/2, camera.viewportHeight/2, 0);
+		
 	}
 	public void createStage(){
 		stage = new TGC_Stage();
@@ -193,6 +211,9 @@ public class TGC_Engine extends Game {
 	}
 	@Override
 	public void render () {
+		camera.update();
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		//if the manager is not done updating, it will display a loading image
 		batch.begin();
 		batch.draw((Texture) manager.get("MainScreen_Splash.png"), 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
@@ -226,7 +247,14 @@ public class TGC_Engine extends Game {
 			screenTransitionTimeLeft -= Gdx.graphics.getDeltaTime();
 		}
 		//print the FPS to system out
-		System.out.println(Gdx.graphics.getFramesPerSecond());
+		//System.out.println(Gdx.graphics.getFramesPerSecond());
 		stage.draw();
 	}
+	
+	@Override
+	public void resize(int width, int height) {
+		viewport.update(width, height);
+		camera.position.set(camera.viewportWidth/2, camera.viewportHeight/2, 0);
+	}
+	
 }
