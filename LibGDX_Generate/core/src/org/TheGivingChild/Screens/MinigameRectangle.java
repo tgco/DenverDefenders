3925712023b768ebeec9a2c55c3bfff4a7ef76eb
mtzimour <mtzimour@CT156-29.adit.mines.edu>
2015-06-09 16:21:00 -0600

    children now follow

---
+++ LibGDX_Generate/core/src/org/TheGivingChild/Screens/MinigameRectangle.java
@@ -0,0 +1,39 @@
+package org.TheGivingChild.Screens;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Rectangle;
+
+public class MinigameRectangle extends Rectangle {
+
+	private boolean occupied;
+	private ChildSprite occupant;
+	
+	public MinigameRectangle(float x, float y, float width, float height) {
+		super(x,y,width,height);
+	}
+	
+	public boolean isOccupied()
+	{
+		return occupied;
+	}
+	
+	public ChildSprite getOccupant()
+	{
+		//return new ChildSprite(occupant);
+		return occupant;
+	}
+	
+	public void setOccupied(ChildSprite s)
+	{
+		//occupant = new ChildSprite(s);
+		occupant = s;
+		occupied = true;
+	}
+	
+	public void empty()
+	{
+		occupied = false;
+		occupant = null;
+	}
+	
+}
