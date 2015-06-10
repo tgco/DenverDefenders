ab398ec3c6113ea06f1e91f3f2b746af6ff58ea0
JackWesleyNelson <janelson@mymail.mines.edu>
2015-06-09 22:00:35 -0600

    Working on adding a new maze

--- LibGDX_Generate/core/src/org/TheGivingChild/Screens/ScreenMaze.java
+++ LibGDX_Generate/core/src/org/TheGivingChild/Screens/ScreenMaze.java
@@ -26,11 +26,11 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 /**
-	*Maze screen that the user will navigate around.
-	*Player will be able to trigger a miniGame by finding a child in the maze.
-	*@author mtzimour
-	*/
-	
+ *Maze screen that the user will navigate around.
+ *Player will be able to trigger a miniGame by finding a child in the maze.
+ *@author mtzimour
+ */
+
 public class ScreenMaze extends ScreenAdapter implements InputProcessor{
 	/** Map to be displayed */
 	private TiledMap map;
@@ -38,7 +38,7 @@ public class ScreenMaze extends ScreenAdapter implements InputProcessor{
 	private OrthographicCamera camera;
 	/** Tiled map renderer to display the map */
 	private TiledMapRenderer mapRenderer;
-	/** Sprite, SpriteBatch, and Textrue for users sprite */
+	/** Sprite, SpriteBatch, and Texture for users sprite */
 	private SpriteBatch spriteBatch;
 	private Texture spriteTexture;
 	private ChildSprite playerCharacter;
@@ -54,46 +54,28 @@ public class ScreenMaze extends ScreenAdapter implements InputProcessor{
 	private Array<MinigameRectangle> minigameRects = new Array<MinigameRectangle>();
 	/** Vector to store the last touch of the user */
 	private Vector2 lastTouch = new Vector2();
-	
+
 	private TGC_Engine game;
 	private AssetManager manager;
-	
+
 	private Array<ChildSprite> mazeChildren;
 	private Array<ChildSprite> followers;
 	private MinigameRectangle miniRec;
-		
-	
+
+
 	/**
 	 * Creates a new maze screen and draws the players sprite on it.
 	 * Sets up map properties such as dimensions and collision areas
 	 * @param mazeFile The name of the maze file in the assets folder
 	 * @param spriteFile The name of the sprite texture file in the assets folder
 	 */
-	
+
 	public ScreenMaze()
 	{
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
 
-		
-
+		//map = new TmxMapLoader().load("mapAssets/SampleUrban.tmx");
+		map = new TmxMapLoader().load("mapAssets/UrbanMaze1.tmx");
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false,w,h);
-		camera.update();
-		map = new TmxMapLoader().load("mapAssets/SampleUrban.tmx");
-		mapRenderer = new OrthogonalTiledMapRenderer(map);
-				
-		spriteBatch = new SpriteBatch();
-		spriteTexture = new Texture(Gdx.files.internal("ball.png"));
-		
-		playerCharacter = new ChildSprite(spriteTexture);
-		playerCharacter.setSpeed(Gdx.graphics.getHeight()/4);
-		
-		mazeChildren = new Array<ChildSprite>();
-		followers = new Array<ChildSprite>();
-		//make sure all the layers are set to visible by default.
-		//this is the source of error for the 'missing' background tiles
-		
 		//Setup map properties
 		properties = map.getProperties();
 		mapTilesX = properties.get("width", Integer.class);
@@ -101,9 +83,25 @@ public class ScreenMaze extends ScreenAdapter implements InputProcessor{
 		//width and height of tiles in pixels
 		int pixWidth = properties.get("tilewidth", Integer.class);
 		int pixHeight = properties.get("tileheight", Integer.class);
-		
+
 		mazeWidth = mapTilesX * pixWidth;
 		mazeHeight = mapTilesY * pixHeight;
+		camera.setToOrtho(false,pixWidth*16,pixHeight*10);
+		camera.update();
+		mapRenderer = new OrthogonalTiledMapRenderer(map);
+
+		spriteBatch = new SpriteBatch();
+		spriteTexture = new Texture(Gdx.files.internal("ball.png"));
+
+		playerCharacter = new ChildSprite(spriteTexture);
+		playerCharacter.setSpeed(Gdx.graphics.getHeight()/4);
+		playerCharacter.setScale(.2f);
+		//Get the rect for the heros headquarters
+		RectangleMapObject startingRectangle = (RectangleMapObject)map.getLayers().get("HeroHeadquarters").getObjects().get(0);
+		playerCharacter.setPosition(startingRectangle.getRectangle().x, startingRectangle.getRectangle().y-startingRectangle.getRectangle().height/2);
+		
+		mazeChildren = new Array<ChildSprite>();
+		followers = new Array<ChildSprite>();
 		
 		//Setup array of collision rectangles
 		MapObjects collisionObjects = map.getLayers().get("Collision").getObjects();
@@ -113,34 +111,34 @@ public class ScreenMaze extends ScreenAdapter implements InputProcessor{
 			Rectangle rect = obj.getRectangle();
 			collisionRects.add(new Rectangle(rect.x, rect.y, rect.width, rect.height));
 		}
-		
+
 		//Setup array of minigame rectangles
 		MapObjects miniGameObjects = map.getLayers().get("Minigame").getObjects();
 		for(int i = 0; i <miniGameObjects.getCount(); i++)
 		{
 			RectangleMapObject obj = (RectangleMapObject) miniGameObjects.get(i);
 			Rectangle rect = obj.getRectangle();
-			
+
 			Rectangle childRec = new Rectangle(rect.x, rect.y, rect.width, rect.height);
 			miniRec = new MinigameRectangle(rect.x, rect.y, rect.width, rect.height);
-			
-			
-			
+
+
+
 			//Add children to be drawn where minigames can be triggered
 			Texture childTexture = new Texture(Gdx.files.internal("mapAssets/somefreesprites/Character Pink Girl.png"));
 			ChildSprite child = new ChildSprite(childTexture);
 			child.setPosition(rect.x - child.getWidth()/4, rect.y - child.getHeight()/4);
 			child.setRectangle(childRec);
 			mazeChildren.add(child);
-			
+
 			miniRec.setOccupied(child);
-			
+
 			minigameRects.add(miniRec);
 		}
-		
+
 		//Remove a child at random so there is always an open spot
 		//children.removeIndex(MathUtils.random(children.size));
-		
+
 		game = ScreenAdapterManager.getInstance().game;
 		manager = game.getAssetManager();
 		game.setScreenSwitch(true);
@@ -152,7 +150,7 @@ public class ScreenMaze extends ScreenAdapter implements InputProcessor{
 	 * bounds of the maze and not on a collision, if so allow it to move.
 	 * If the player triggers a miniGame set that to the current screen.
 	 */
-	
+
 	@Override 
 	public void render(float delta)
 	{
@@ -163,8 +161,8 @@ public class ScreenMaze extends ScreenAdapter implements InputProcessor{
 				Gdx.gl.glClearColor(1, 0, 0, 1);
 				Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 				Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-						
-						
+
+
 				//update the camera
 				camera.update();
 				//set the map to be rendered by this camera
@@ -181,15 +179,15 @@ public class ScreenMaze extends ScreenAdapter implements InputProcessor{
 				//Check for a collision as well
 				boolean collision = false;
 				boolean triggerGame = false;
-				
-				
+
+
 				if(spriteMoveX >= 0 && (spriteMoveX+playerCharacter.getWidth()) <= mazeWidth)
 				{
 					if(spriteMoveY >= 0 && (spriteMoveY+playerCharacter.getHeight()) <= mazeHeight)
 					{
-						
+
 						Rectangle spriteRec = new Rectangle(spriteMoveX, spriteMoveY, playerCharacter.getWidth(), playerCharacter.getHeight());
-						
+
 						for(Rectangle r : collisionRects)
 						{
 							if(r.overlaps(spriteRec))
@@ -197,44 +195,44 @@ public class ScreenMaze extends ScreenAdapter implements InputProcessor{
 								collision = true;
 							}
 						}
-						
-																
+
+
 						for(MinigameRectangle m : minigameRects)
 						{
 							if(m.overlaps(spriteRec) && m.isOccupied())
 							{
-								
+
 								followers.add(m.getOccupant());
 								m.empty();
 								playerCharacter.setPosition(m.getX(), m.getY());
 								triggerGame = true;
 								ScreenAdapterManager.getInstance().show(ScreenAdapterEnums.LEVEL);
-														
+
 							}
 						}
-										
+
 						if(!collision){
 							playerCharacter.setPosition(spriteMoveX, spriteMoveY);
-							
+
 							if(followers.size > 0)
 							{
 								followers.get(0).followSprite(playerCharacter);
 								System.out.println("set first to follow");
-								
-								
+
+
 								for(int i = 1; i <followers.size; i++)
 								{
 									followers.get(i).followSprite(followers.get(i-1));
 									System.out.println("set follow for" + followers.get(i).toString());
 								}
-								
+
 							}
-							
+
 						}
-						
+
 					}	
 				}
-				
+
 				//begin the batch that sprites will draw to
 				spriteBatch.begin();
 				//draw the main character sprite to the map
@@ -242,14 +240,14 @@ public class ScreenMaze extends ScreenAdapter implements InputProcessor{
 				//Draw the children following
 				if(followers.size != 0)
 				{
-				for(Sprite f: followers)
-				{
-					//System.out.println(followers.size);
-					//System.out.println(f.get);
-					f.draw(spriteBatch);
+					for(Sprite f: followers)
+					{
+						//System.out.println(followers.size);
+						//System.out.println(f.get);
+						f.draw(spriteBatch);
+					}
 				}
-				}
-				
+
 				//draw the children on the maze
 				for(Sprite s : mazeChildren)
 				{
@@ -259,15 +257,15 @@ public class ScreenMaze extends ScreenAdapter implements InputProcessor{
 				camera.position.set(playerCharacter.getX(), playerCharacter.getY(), 0);
 				//end the batch that sprites have drawn to
 				spriteBatch.end();
-				
-				
+
+
 			}
 		}
 		if(ScreenAdapterManager.getInstance().SCREEN_TRANSITION_TIME_LEFT >= 0)
 			ScreenAdapterManager.getInstance().SCREEN_TRANSITION_TIME_LEFT -= Gdx.graphics.getDeltaTime();
 	}
-	
-	
+
+
 	@Override
 	public boolean keyDown(int keycode) {
 		//need key down to have key up function
@@ -276,7 +274,7 @@ public class ScreenMaze extends ScreenAdapter implements InputProcessor{
 
 	@Override
 	public boolean keyUp(int keycode) {
-		
+
 		return true;
 	}
 
@@ -294,7 +292,7 @@ public class ScreenMaze extends ScreenAdapter implements InputProcessor{
 	 * @param pointer not used
 	 * @param button not used
 	 */
-	
+
 	@Override
 	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
 		//Get vector of touch down to calculate change in movement during swipe
@@ -312,7 +310,7 @@ public class ScreenMaze extends ScreenAdapter implements InputProcessor{
 	 * @param pointer not used
 	 * @param button not used
 	 */
-	
+
 	@Override
 	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
 		//vectors contain the new position where the swipe ended
@@ -320,7 +318,7 @@ public class ScreenMaze extends ScreenAdapter implements InputProcessor{
 		//calculate the difference between the begin and end point
 		Vector2 delta = newTouch.cpy().sub(lastTouch);
 		//if the magnitude of x is greater than the y, then move the sprite in the horizontal direction
-		
+
 		if (Math.abs(delta.x) > Math.abs(delta.y))
 		{
 			//if the change was positive, move right, else move left
@@ -328,7 +326,7 @@ public class ScreenMaze extends ScreenAdapter implements InputProcessor{
 			if(delta.x <= 0) xMove = -playerCharacter.getSpeed();
 			//no vertical movement
 			yMove = 0;
-						
+
 		}
 		//otherwise y>=x so move vertically 
 		else
@@ -339,13 +337,13 @@ public class ScreenMaze extends ScreenAdapter implements InputProcessor{
 			//no horizontal movement
 			xMove = 0;
 		}
-		
+
 		playerCharacter.setMove(xMove, yMove);
-		
+
 		return true;
-				
+
 	}
-	
+
 	/**
 	 * Sets the maze to be shown, makes sure the player is not moving initially.
 	 * Sets all layers of the maze to visible. 
@@ -357,29 +355,29 @@ public class ScreenMaze extends ScreenAdapter implements InputProcessor{
 		game.loadLevelPackets();
 		xMove = 0;
 		yMove = 0;
-		
+
 		for(MapLayer layer: map.getLayers()){
 			layer.setVisible(true);
 		}
 		MapObjects collisionObjects = map.getLayers().get("Collision").getObjects();
-		
+
 		for(int i = 0; i <collisionObjects.getCount(); i++)
 		{
 			RectangleMapObject obj = (RectangleMapObject) collisionObjects.get(i);
 			Rectangle rect = obj.getRectangle();
 			collisionRects.add(new Rectangle(rect.x, rect.y, rect.width, rect.height));
 		}
-		
+
 
 		Gdx.input.setInputProcessor(this);
 
 	}
-	
+
 	/**
 	 * Hides the maze by setting all layers to not visible.
 	 * Sets input processor back to the stage.
 	 */
-	
+
 	@Override
 	public void hide(){
 		for(MapLayer layer: map.getLayers()){
@@ -389,7 +387,7 @@ public class ScreenMaze extends ScreenAdapter implements InputProcessor{
 		collisionRects.clear();
 		game.setScreenSwitch(true);
 	}
-	
+
 	@Override
 	public boolean mouseMoved(int screenX, int screenY) {
 		// TODO Auto-generated method stub
